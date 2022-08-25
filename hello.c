a546e42167db9e1c4a6dbcdf5e416cc73dc447dd
Trayson Combs <trayson.combs@gmail.com>
2022-08-25 10:54:45 -0500

    comments

--- hello.c
+++ hello.c
@@ -1,4 +1,8 @@
 #include <stdio.h>
+/* very original program that [rints name and Major
+ * 2022/08/25
+ * By: Trayson Combs
+ */
 int main() {
     printf("Trayson!\n");
     printf("Computer Science");
